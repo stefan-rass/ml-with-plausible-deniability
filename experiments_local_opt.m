1c6abc019c5ec482e0a2463f42630f04fd6e1bfb
Markus <vogl91@gmail.com>
2024-03-05 02:24:48 +0100

    Create python version for reproducability.

--- experiments_local_opt.m
+++ experiments_local_opt.m
@@ -1,7 +1,11 @@
 % experimental test for local optimality
 
 clear all   % avoid side-effects upon several executions
-pkg load optim  % for nonlinear optimization 
+pkg load optim  % for nonlinear optimization
+format shortG
+# fun fact: this breaks with 42
+rand("state", 41)  % make the random number generator reproducible
+
 
 % test with reasonably small dimensions; scalability is indeed an issue, not the 
 % least so because the numeric optimization becomes much harder. Our goal is using
@@ -26,6 +30,8 @@ if (cvg < 0)
   disp('1st optimization failed')
 endif
 
+p  % debug print
+
 x_decoy = randi(8,n,m)  % this shall be the data that we seek to make plausible
 % note that we now pick the response also at random!
 %y_decoy = (-1/2*log(rand(n,1)))   % sample from the same distribution (plausibility) but with a different parameter
@@ -43,7 +49,11 @@ else
   % the rank condition was satisfied
   disp('rank condition OK; constructing the norm')
   B = (null(e'))';
-  
+
+  % print the basis without any line break so it's easier to read again
+  disp("B =\n")
+  fprintf(1, [repmat(' %9.5f', 1, n) '\n'], B')
+
   % constructing a linearly independent subspace of dimension 1
   w1 = e;
   w1(1) = w1(1) + 1;  % small distortion of e should be enough
@@ -51,18 +61,32 @@ else
   while(rank([w1 e]) < 2 || rank(B) >= rank([B;w1']))
     w1 = rand(size(e))  % retry with a random vector (should succeed with probability 1)
   endwhile
-  w1 = w1 / sum(w1);  % scale to unit length
+  w1 = w1 / sum(w1)  % scale to unit length, then debug print
 
   disp('re-fitting')
   % re-fit with the new norm functional
-  p0 = p + 0.1*rand;  % start within a proximity; *local* optimality only (as our model is linear, we could - in theory - have used any starting point, though)
+  rand_mul = rand  % debug print the random scaling factor
+  p0 = p + 0.1*rand_mul;  % start within a proximity; *local* optimality only (as our model is linear, we could - in theory - have used any starting point, though)
   [p2, objf, cvg, outp] = nonlin_min(@(beta) (crafted_norm(beta(1) + x_decoy*beta(2:d) - y_decoy, B, w1)), p0);
   if (cvg < 0) 
     disp('2nd optimization failed')
   endif
 
-  % the newly found result should be (roughly) equal to the original parameter vector 
+  p2  % debug print the p2
+
+  % the newly found result should be (roughly) equal to the original parameter vector
   % (up to perhaps numeric inaccuracies, but approximately recover p)
   disp('comparing the results: [original | newly fitted]')
   disp([p p2])
+
+  disp("y_training, y_training_rec_orig, y_training_rec_fit")
+  y_training_rec_orig = p(1) + x_training * p(2:d);
+  y_training_rec_fit = p2(1) + x_training * p2(2:d);
+  disp([y_training y_training_rec_orig y_training_rec_fit])
+
+  disp("y_decoy, y_decoy_rec_orig, y_decoy_rec_fit")
+  y_decoy_rec_orig = p(1) + x_decoy * p(2:d);
+  y_decoy_rec_fit = p2(1) + x_decoy * p2(2:d);
+  disp([y_decoy y_decoy_rec_orig y_decoy_rec_fit])
+
 endif
